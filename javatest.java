951421d683f41602f95446f5457464f7a30e3aed
alisekkei <alizairullah1996@gmail.com>
2021-11-24 12:55:40 +0800

    Add Name in Line Java
    Just put my name

--- javatest.java
+++ javatest.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-	System.out.println("Im, New Here!"); 
-    }
+	System.out.println("Im, New Here!");
+	System.out.println("My Name is Mohd Ali Zairullah bin Alimun");     }
 }
